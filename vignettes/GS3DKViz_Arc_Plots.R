d7ec28e505fb39987612dd1805bd6304696e947e
James Dalgleish <jamesdalg@gmail.com>
2020-12-22 23:37:05 -0500

    Vignette data

---
+++ vignettes/GS3DKViz_Arc_Plots.R
@@ -0,0 +1,8 @@
+## ----setup, include=T,message=F,error=F,warning=F-----------------------------
+knitr::opts_chunk$set(echo = TRUE,message=F,error=F,warning=F)
+knitr::opts_chunk$set(root.dir="./inst/extdata/")
+library(Gviz)
+library(GenomicInteractions)
+library(magrittr)
+library(GS3DKViz)
+
