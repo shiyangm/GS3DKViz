b6f34b1511a7992a2c6d401a07dcc7367c5d00db
James Dalgleish <jamesdalg@gmail.com>
2020-12-29 13:53:23 -0500

    Fixed vignettes, added detail function, cleared notes.

--- R/plotInteractionDetail.R
+++ R/plotInteractionDetail.R
@@ -2,33 +2,33 @@
 #' 
 #' Creates a Gviz promoter-enhancer plot
 #' 
-#' @name plotInteraction
+#' @name plotInteractionDetail
 #' @keywords GenomicRanges GenomicInteractions metadata Gviz
 #' @param gint genomic ranges object on a single chromosome
-#' @param extra_prom_gr A Genomic Ranges
-#' object containing extra promoters that are disconnected from interactions
+#' @param extra_prom_gr A Genomic Ranges object containing extra promoters that are disconnected from interactions
 #' @param chr chromsome
 #' @param bounds_offset Beyond the largest and smallest elements, how much extra space in bp should be plotted?
 #' @param main character string for the title of the plot
 #' @importFrom Gviz IdeogramTrack AnnotationTrack GenomeAxisTrack displayPars
 #' @importFrom Gviz feature feature<- plotTracks
 #' @importFrom S4Vectors mcols
-#' @importFrom GenomicInteractions InteractionTrack
+#' @importFrom GenomicInteractions InteractionTrack annotateInteractions
 #' @importFrom rlang .data
 #' @importFrom tibble as_tibble
 #' @importFrom dplyr filter pull
 #' @examples
 #' #See example at the start of the vignette
 #' @export
-plotInteractionDetail=function(gint,chr,bounds_offset=1.5e4,main=NULL) {
+plotInteractionDetail=function(gint,extra_prom_gr,chr,bounds_offset=1.5e4,main=NULL) {
   promoter_gr=anchorOneWithMetadata(gint)
   enhancer_gr=anchorTwoWithMetadata(gint)
-  annotateInteractions(GIObject = gint,annotations =  list(promoter=promoter_gr,
+  GenomicInteractions::annotateInteractions(GIObject = gint,annotations =  list(promoter=promoter_gr,
                                     enhancer=enhancer_gr),id.col="gene_id")
   knockoff_genes=S4Vectors::mcols(gint) %>% tibble::as_tibble() %>%
-    dplyr::filter(identified_by_knockoff_or_not=="Yes") %>% dplyr::pull(gene_id)
-  gs3dk_gint@regions@elementMetadata$node.class=(S4Vectors::mcols(regions(
-    gs3dk_gint)) %>% 
+    dplyr::filter(.data$identified_by_knockoff_or_not=="Yes") %>% dplyr::pull(gene_id)
+  
+  gint@regions@elementMetadata$node.class=(S4Vectors::mcols(regions(
+    gint)) %>% 
   tibble::as_tibble() %>%
   dplyr::mutate(knockoff=ifelse((promoter.id %in% knockoff_genes | 
   enhancer.id %in% knockoff_genes),"knockoff_detected","knockoff_removed"),
@@ -36,9 +36,9 @@ plotInteractionDetail=function(gint,chr,bounds_offset=1.5e4,main=NULL) {
     dplyr::pull(knockoff))
   interaction_track <- InteractionTrack(gint,
   name = "Interaction",
-  chromosome = as.character(gs3dk_gint %>% as.data.frame() %>%
-      janitor::clean_names() %>% dplyr::filter(trait=="AD") %>%
-        dplyr::pull(seqnames1)))
+  chromosome = as.character(gint %>% as.data.frame() %>%
+      janitor::clean_names() %>% dplyr::filter(.data$trait=="AD") %>%
+        dplyr::pull(.data$seqnames1)))
   displayPars(interaction_track)=list(col.interactions="black",
     col.interaction.types=c('knockoff_detected-knockoff_detected'='blue',
         'knockoff_detected-knockoff_removed'='blue',
@@ -83,7 +83,7 @@ plotInteractionDetail=function(gint,chr,bounds_offset=1.5e4,main=NULL) {
   displayPars(deTrackEnh2) <- list(fill = "mediumpurple1", col = NA, 
                                    fontcolor.feature = "black", fontsize=10,
                                    just.group="below",rotation.item=90,
-                                   collapse=T,mergeGroups=T,showOverplotting=T,groupAnnotation="group",    group=mcols(gs3dk_gint)$enhancer_type,feature=mcols(gs3dk_gint)$enhancer_type,
+                                   collapse=T,mergeGroups=T,showOverplotting=T,groupAnnotation="group",    group=mcols(gint)$enhancer_type,feature=mcols(gint)$enhancer_type,
                                    groupDetails=T,detailsConnector.lty="solid",
                                    detailsConnector.col="black",
                                    detailsBorder.col="black",
@@ -108,10 +108,10 @@ plotInteractionDetail=function(gint,chr,bounds_offset=1.5e4,main=NULL) {
   # )
   # 
   # Gviz::displayPars(interaction_track)=list(col.interactions="black")
-  bounds=c(gint %>% as.data.frame() %>% janitor::clean_names()  %>% dplyr::pull(.data$start1) %>% min(),
-           gint %>% as.data.frame() %>% janitor::clean_names()  %>% dplyr::pull(.data$end1) %>% max(),
-           gint %>% as.data.frame() %>% janitor::clean_names()  %>% dplyr::pull(.data$start2) %>% min(),
-           gint %>% as.data.frame() %>% janitor::clean_names()  %>% dplyr::pull(.data$end2) %>% max())
+  bounds=c(gint[mcols(gint)$seqnames1==chr] %>% as.data.frame() %>% janitor::clean_names()  %>% dplyr::pull(.data$start1) %>% min(),
+           gint[mcols(gint)$seqnames1==chr] %>% as.data.frame() %>% janitor::clean_names()  %>% dplyr::pull(.data$end1) %>% max(),
+           gint[mcols(gint)$seqnames1==chr] %>% as.data.frame() %>% janitor::clean_names()  %>% dplyr::pull(.data$start2) %>% min(),
+           gint[mcols(gint)$seqnames1==chr] %>% as.data.frame() %>% janitor::clean_names()  %>% dplyr::pull(.data$end2) %>% max())
   
   Gviz::plotTracks(list(itrack,gtrack,interaction_track,CombinedPromoterTrack,
                         deTrackEnh2),
@@ -125,7 +125,8 @@ plotInteractionDetail=function(gint,chr,bounds_offset=1.5e4,main=NULL) {
              fontsize=18,fontsize.item=7,collapse=T,min.width=5,mergeGroups=T
              ,stacking="dense" #comment out to restore stacking
              ,main=main,
-             background.title = "black"
+             background.title = "black",
+             unconnected="orange",connected="green"
   )
   
  return(NULL) 
